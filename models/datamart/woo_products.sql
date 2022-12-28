8f0d14f762a406ec4646650293f85137dc9ff240
beefa <mohammed.kalimulla@lsnsoft.in>
2022-12-28 18:20:06 +0530

    kalim

--- models/datamart/woo_products.sql
+++ models/datamart/woo_products.sql
@@ -1,1 +1,1 @@
-select name as title, tags, 'null' as image ,images ,status ,description ,date_created as created_at ,date_modified as updated_at ,permalink ,variants ,attributes from {{ ref('wc_products') }}
+select name as title, tags, 'null' as image ,images ,status ,description ,date_created as created_at ,date_modified as updated_at ,permalink ,"null" as variants ,attributes from {{ ref('wc_products') }}
