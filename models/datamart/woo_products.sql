2bc7ae36ac8d062ba02e4591aeff784231bc827f
beefa <mohammed.kalimulla@lsnsoft.in>
2022-12-28 18:16:22 +0530

    products

--- models/datamart/woo_products.sql
+++ models/datamart/woo_products.sql
@@ -1,1 +1,1 @@
-select * from {{ ref('wc_products') }}
+select name as title, tags, 'null' as image ,images ,status ,description ,date_created as created_at ,date_modified as updated_at ,permalink ,variants ,attributes from {{ ref('wc_products') }}
